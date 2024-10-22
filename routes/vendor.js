5765fd4c5b792807bf523a34a14faad72c1bbbd7
Jacob Delanyo Dotsey <dotseydelanyo11@gmail.com>
2024-10-22 09:29:04 +0000

    vendor

--- routes/vendor.js
+++ routes/vendor.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {registerVendor, loginVendor, logoutVendor, getProfile, updateProfile } from "../controllers/vendor.js";
+import { hasPermission, isAuthenticated } from "../middlewares/auth.js";
 
 
 // create routes
@@ -11,11 +12,11 @@ vendorRouter.post('/vendors/register', registerVendor)
 
 vendorRouter.post('/vendors/login', loginVendor)
 
-vendorRouter.get('/vendors/me', getProfile)
+vendorRouter.get('/vendors/me', isAuthenticated, hasPermission('get_profile'), getProfile)
 
-vendorRouter.post('/vendors/logout', logoutVendor)
+vendorRouter.post('/vendors/logout', isAuthenticated, logoutVendor)
 
-vendorRouter.post('/vendors/me', updateProfile)
+vendorRouter.patch('/vendors/me', isAuthenticated, hasPermission('update_profile'), updateProfile)
 
 // export default
 export default vendorRouter;

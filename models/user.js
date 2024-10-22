9349792f7e8bc1f0a8cd9cf4c7dc8cf4cda6bd30
Davida <byourself77by@gmail.com>
2024-10-22 09:35:01 +0000

    Add user auth

--- models/user.js
+++ models/user.js
@@ -5,11 +5,11 @@ const userSchema = new Schema({
     username: {type: String, required: true},
     location: { type: String},
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    role: { type: String, enum: ['vendor', 'user'], default: 'user' }
 }, {
     timestamps: true
-}
-)
+})
 
 userSchema.plugin(toJSON);
 export const UserModel = model('User', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 //import express router
 import { Router } from "express";
-import { userAds, userRegister, userLogin } from "../controllers/user.js";
+import { userAds, userRegister, userLogin, userLogout } from "../controllers/user.js";
+import { isAuthenticated, userHasPermission } from "../middlewares/auth.js";
+
 
 //create a router
 const userRouter = Router();
@@ -10,7 +12,7 @@ userRouter.get('/users/ads', userAds);
 userRouter.get('./users/ad',);
 userRouter.post('/users/register', userRegister);
 userRouter.post('/users/login', userLogin);
-userRouter.post('/users/logout',);
+userRouter.post('/users/logout', isAuthenticated, userLogout);
 
 //export user 
 export default userRouter;

1333d7fac7eb04e211ab8e61539e624d64619053
Jacob Delanyo Dotsey <dotseydelanyo11@gmail.com>
2024-10-24 13:34:10 +0000

    comment limit out

--- controllers/advert.js
+++ controllers/advert.js
@@ -26,7 +26,7 @@ export const addAdvert = async (req, res, next) => {
 
 export const getAdverts = async (req, res, next) => {
     try {
-        const { filter = "{}", sort = "{}", limit = 10, skip = 0 } = req.query;
+        const { filter = "{}", sort = "{}", /** limit = 10, skip = 0 */} = req.query;
         //  fetch adverts from database
         const adverts = await AdvertModel
             .find(JSON.parse(filter))
